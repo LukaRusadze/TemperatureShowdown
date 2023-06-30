242f487c8fe69f59bcb7ea97120d3a0fe868638f
LukaRusadze <rusadzeluka@gmail.com>
2023-07-01 01:59:56 +0400

    feat: initial HomeScreen design

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,16 +1,34 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import React from 'react';
+import { MainStackScreenProps } from '@navigation';
+import { Button } from '@primitives';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useGameState } from '@context';
 
-interface Props {}
+type Props = MainStackScreenProps<'Home'>;
 
-const HomeScreen = (props: Props) => {
+const HomeScreen = ({}: Props) => {
+  const gameInstance = useGameState();
+
+  const handleGameStart = (difficulty: GameDifficultyLevel) => () => {
+    gameInstance.chooseDifficulty(difficulty);
+  };
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Button title={'Easy'} onPress={handleGameStart('Easy')} />
+      <Button title={'Medium'} onPress={handleGameStart('Medium')} />
+      <Button title={'Hard'} onPress={handleGameStart('Hard')} />
+    </SafeAreaView>
   );
 };
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+});
