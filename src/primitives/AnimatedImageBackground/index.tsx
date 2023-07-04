52dda1c3aeec0042741f66b95cedabe2eb845d4d
LukaRusadze <rusadzeluka@gmail.com>
2023-07-05 02:52:26 +0400

    chore: new animated components in separated

---
+++ src/primitives/AnimatedImageBackground/index.tsx
@@ -0,0 +1,4 @@
+import { ImageBackground } from 'react-native';
+import Animated from 'react-native-reanimated';
+
+export default Animated.createAnimatedComponent(ImageBackground);
--- src/primitives/index.ts
+++ src/primitives/index.ts
@@ -1,1 +1,3 @@
+export { default as AnimatedImageBackground } from './AnimatedImageBackground';
+export { default as AnimatedPressable } from './AnimatedPressable';
 export { default as Button } from './Button';
